b1225c41aa4bd0240983f01c8560bb3bc1131ce0
Eduardo Quesquen <equesquensa@gmail.com>
2021-01-19 07:09:05 -0500

    Choice Picker

---
+++ script.js
@@ -0,0 +1,85 @@
+const tagsEl = document.getElementById('tags')
+const textarea = document.getElementById('textarea')
+
+textarea.focus()
+
+textarea.addEventListener('keyup', (e) => {
+  createTags(e.target.value)
+
+  if(e.key === 'Enter' && tags.childElementCount > 1){
+    
+    setTimeout(() => {
+      e.target.value = '';
+      textarea.disabled = true;
+    }, 10)
+    
+    randomSelect()
+  }
+})
+
+
+function createTags(input){
+  const tags = input.split(',').filter(tag => tag.trim() !== '').map(tag => tag.trim())
+
+  tagsEl.innerHTML=''
+
+  tags.forEach(tag => {
+    const tagEl = document.createElement('span')
+    tagEl.classList.add('tag')
+    tagEl.innerText = tag
+    tagsEl.appendChild(tagEl)
+  });
+}
+
+function randomSelect(){
+  const times = 30
+
+  const interval = setInterval(() => {
+    const randomTag = pickRandomTag()
+
+    highlightTag(randomTag)
+
+    setTimeout(() => {
+      unhighlightTag(randomTag)
+    },100)
+
+  }, 100);
+
+  setTimeout(() => {
+    clearInterval(interval)
+
+    setTimeout(() => {
+      const randomTag = pickRandomTag()
+      console.log('random picked is', randomTag);
+      
+      const tags = document.querySelectorAll('.tag');
+      tags.forEach((tag) => {
+        if (tag !== randomTag) {
+          tag.classList.add('invis');
+        }
+      });
+
+      highlightTag(randomTag)
+      textarea.disabled = false;
+
+    },100)
+
+  }, times * 100)
+}
+
+function pickRandomTag(){
+  const tags = document.querySelectorAll('.tag')
+  return tags[Math.floor(Math.random() * tags.length)]
+}
+
+function highlightTag(tag){
+  tag.classList.add('highlight')
+}
+
+function unhighlightTag(tag){
+  tag.classList.remove('highlight')
+}
+
+function newhightlightTag(tag){
+  tag.classList.add('new')
+}
